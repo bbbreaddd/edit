9c11d629f9081c0a15eb4ff7ee3ced656c63e20e
bread <136384195+bbbreaddd@users.noreply.github.com>
2026-04-17 01:02:45 -0700

    change highlight icon

--- docs/.vitepress/theme/components/VPLocalSearchBox.vue
+++ docs/.vitepress/theme/components/VPLocalSearchBox.vue
@@ -977,9 +977,15 @@ function onMouseMove(e: MouseEvent) {
               :class="{ 'highlight-active': areSearchHighlightsEnabled }"
               :title="areSearchHighlightsEnabled ? 'Disable Highlights' : 'Enable Highlights'"
               :aria-pressed="areSearchHighlightsEnabled ? 'true' : 'false'"
+              aria-label="Toggle Search Highlights"
               @click="toggleSearchHighlights"
             >
-              <span class="highlight-icon">✦</span>
+              <span class="highlight-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="m9 11-6 6v3h9l3-3"></path>
+                  <path d="m22 12-4.6 4.6a2 2 0 0 1-2.8 0l-5.2-5.2a2 2 0 0 1 0-2.8L14 4"></path>
+                </svg>
+              </span>
             </button>
 
             <button
